9e7254eaf05ae28a8c5f815f5eb39806d7621e34
Alanis3a1b <alanis.alvarez@catolica.edu.sv>
2025-03-31 18:41:40 -0600

    Se modificó la interfaz de menú y la de inventario.

--- Fomularios/VistasAdmin/FormInventarioAdmin.Designer.cs
+++ Fomularios/VistasAdmin/FormInventarioAdmin.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnComidas = new System.Windows.Forms.Button();
+            this.btnExtras = new System.Windows.Forms.Button();
+            this.btnBebidas = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnComidas
+            // 
+            this.btnComidas.BackColor = System.Drawing.Color.Yellow;
+            this.btnComidas.Location = new System.Drawing.Point(472, 91);
+            this.btnComidas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnComidas.Name = "btnComidas";
+            this.btnComidas.Size = new System.Drawing.Size(159, 52);
+            this.btnComidas.TabIndex = 5;
+            this.btnComidas.Text = "Gestionar comidas";
+            this.btnComidas.UseVisualStyleBackColor = false;
+            // 
+            // btnExtras
+            // 
+            this.btnExtras.BackColor = System.Drawing.Color.Yellow;
+            this.btnExtras.Location = new System.Drawing.Point(279, 91);
+            this.btnExtras.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnExtras.Name = "btnExtras";
+            this.btnExtras.Size = new System.Drawing.Size(145, 53);
+            this.btnExtras.TabIndex = 4;
+            this.btnExtras.Text = "Gestionar extras";
+            this.btnExtras.UseVisualStyleBackColor = false;
+            // 
+            // btnBebidas
+            // 
+            this.btnBebidas.BackColor = System.Drawing.Color.Yellow;
+            this.btnBebidas.Location = new System.Drawing.Point(72, 91);
+            this.btnBebidas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBebidas.Name = "btnBebidas";
+            this.btnBebidas.Size = new System.Drawing.Size(159, 52);
+            this.btnBebidas.TabIndex = 3;
+            this.btnBebidas.Text = "Gestionar bebidas";
+            this.btnBebidas.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(72, 41);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 52);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Gestionar proveedores";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.groupBox1.Location = new System.Drawing.Point(51, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(688, 125);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Proveedores";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox2.Controls.Add(this.btnExtras);
+            this.groupBox2.Controls.Add(this.btnBebidas);
+            this.groupBox2.Controls.Add(this.btnComidas);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.groupBox2.Location = new System.Drawing.Point(51, 179);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(688, 229);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Inventario";
+            // 
+            // FormInventarioAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormInventarioAdmin";
             this.Text = "FormInventarioAdmin";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnComidas;
+        private System.Windows.Forms.Button btnExtras;
+        private System.Windows.Forms.Button btnBebidas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
--- Fomularios/VistasAdmin/FormMenuAdmin.Designer.cs
+++ Fomularios/VistasAdmin/FormMenuAdmin.Designer.cs
@@ -31,53 +31,87 @@
             this.btnBebidas = new System.Windows.Forms.Button();
             this.btnExtras = new System.Windows.Forms.Button();
             this.btnComidas = new System.Windows.Forms.Button();
+            this.gbDatosBebida = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbDatosBebida.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnBebidas
             // 
-            this.btnBebidas.Location = new System.Drawing.Point(157, 172);
+            this.btnBebidas.BackColor = System.Drawing.Color.Yellow;
+            this.btnBebidas.Location = new System.Drawing.Point(296, 167);
             this.btnBebidas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnBebidas.Name = "btnBebidas";
-            this.btnBebidas.Size = new System.Drawing.Size(103, 52);
+            this.btnBebidas.Size = new System.Drawing.Size(121, 69);
             this.btnBebidas.TabIndex = 0;
             this.btnBebidas.Text = "Bebidas";
-            this.btnBebidas.UseVisualStyleBackColor = true;
+            this.btnBebidas.UseVisualStyleBackColor = false;
             this.btnBebidas.Click += new System.EventHandler(this.btnBebidas_Click);
             // 
             // btnExtras
             // 
-            this.btnExtras.Location = new System.Drawing.Point(347, 171);
+            this.btnExtras.BackColor = System.Drawing.Color.Yellow;
+            this.btnExtras.Location = new System.Drawing.Point(538, 167);
             this.btnExtras.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnExtras.Name = "btnExtras";
-            this.btnExtras.Size = new System.Drawing.Size(91, 53);
+            this.btnExtras.Size = new System.Drawing.Size(109, 70);
             this.btnExtras.TabIndex = 1;
             this.btnExtras.Text = "Extras";
-            this.btnExtras.UseVisualStyleBackColor = true;
+            this.btnExtras.UseVisualStyleBackColor = false;
             this.btnExtras.Click += new System.EventHandler(this.btnExtras_Click);
             // 
             // btnComidas
             // 
-            this.btnComidas.Location = new System.Drawing.Point(540, 172);
+            this.btnComidas.BackColor = System.Drawing.Color.Yellow;
+            this.btnComidas.Location = new System.Drawing.Point(67, 168);
             this.btnComidas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnComidas.Name = "btnComidas";
-            this.btnComidas.Size = new System.Drawing.Size(99, 52);
+            this.btnComidas.Size = new System.Drawing.Size(117, 69);
             this.btnComidas.TabIndex = 2;
             this.btnComidas.Text = "Comidas";
-            this.btnComidas.UseVisualStyleBackColor = true;
+            this.btnComidas.UseVisualStyleBackColor = false;
             this.btnComidas.Click += new System.EventHandler(this.btnComidas_Click);
+            // 
+            // gbDatosBebida
+            // 
+            this.gbDatosBebida.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.gbDatosBebida.Controls.Add(this.label1);
+            this.gbDatosBebida.Controls.Add(this.btnComidas);
+            this.gbDatosBebida.Controls.Add(this.btnBebidas);
+            this.gbDatosBebida.Controls.Add(this.btnExtras);
+            this.gbDatosBebida.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbDatosBebida.Location = new System.Drawing.Point(33, 40);
+            this.gbDatosBebida.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gbDatosBebida.Name = "gbDatosBebida";
+            this.gbDatosBebida.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gbDatosBebida.Size = new System.Drawing.Size(711, 355);
+            this.gbDatosBebida.TabIndex = 7;
+            this.gbDatosBebida.TabStop = false;
+            this.gbDatosBebida.Text = "Menú";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(150, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seleccione la parte del menú a gestionar";
             // 
             // FormMenuAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnComidas);
-            this.Controls.Add(this.btnExtras);
-            this.Controls.Add(this.btnBebidas);
+            this.Controls.Add(this.gbDatosBebida);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormMenuAdmin";
             this.Text = "FormMenuAdmin";
             this.Load += new System.EventHandler(this.FormMenuAdmin_Load);
+            this.gbDatosBebida.ResumeLayout(false);
+            this.gbDatosBebida.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -87,5 +121,7 @@
         private System.Windows.Forms.Button btnBebidas;
         private System.Windows.Forms.Button btnExtras;
         private System.Windows.Forms.Button btnComidas;
+        private System.Windows.Forms.GroupBox gbDatosBebida;
+        private System.Windows.Forms.Label label1;
     }
 }
